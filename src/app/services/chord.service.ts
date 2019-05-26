73f107835618a0806070b1df9b9157fb91fe8421
Maz Hermon <mazhermon@gmail.com>
2019-05-26 14:37:21 +1200

    clean up mem leak in chord service

--- src/app/services/chord.service.ts
+++ src/app/services/chord.service.ts
@@ -3,6 +3,7 @@ import { ModesService } from './modes.service';
 import { KeysService } from './keys.service';
 import { Injectable } from '@angular/core';
 import { Subject, of, Observable } from 'rxjs';
+import { take } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -47,7 +48,9 @@ export class ChordService {
       let randomMode = this._getRandomMode();
       //_currentChordsChecker.push(this.setChord(chord.numeral, key, this._getCurrentMode()));
       // refactor how get current mode is working on previous line
-      this.setChord(chord.numeral, key, randomMode).subscribe(chord => (newChords.push(chord)));
+      this.setChord(chord.numeral, key, randomMode).pipe(
+        take(1)
+      ).subscribe(chord => (newChords.push(chord)));
     }
 
     return of(newChords);
