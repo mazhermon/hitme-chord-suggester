0ac5fe0621be812d79766cfae9b7c37145dc375e
Maz Hermon <mazhermon@gmail.com>
2019-05-31 11:52:24 +1200

    add some more KEYS and scale data to get started

--- src/app/data/keys.data.ts
+++ src/app/data/keys.data.ts
@@ -1,12 +1,28 @@
 import { Key } from '../models/key.model';
 
 export const KEYS: Key[] = [
-    { 
+    {
         name: 'C',
-        scale: ['C','D','E','F','G','A','B']
+        scale: ['C', 'D', 'E', 'F', 'G', 'A', 'B']
+    },
+    {
+        name: 'C#',
+        scale: ['C#', 'D#', 'E#', 'F#', 'G#', 'A#', 'B#']
+    },
+    {
+        name: 'Db',
+        scale: ['Db', 'Eb', 'F', 'Gb', 'Ab', 'Bb', 'C']
     },
     {
         name: 'D',
-        scale: ['D','E','F#','G','A','B','C#'] 
+        scale: ['D', 'E', 'F#', 'G', 'A', 'B', 'C#']
+    },
+    {
+        name: 'Eb',
+        scale: ['Eb', 'F', 'G', 'Ab', 'Bb', 'C', 'D']
+    },
+    {
+        name: 'E',
+        scale: ['E', 'F#', 'G#', 'A', 'B', 'C#', 'D#']
     }
 ];
